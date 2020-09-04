761eb0e72dcf303a2220a8deec705dcdcc870d5c
Anton Ruettinger <Anton@Antons-Air.fritz.box>
2020-09-04 15:54:05 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, meine Seite ist gerade am Entstehen !<span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>Hier geht's zu meinen Blogs - ebenfalls noch under Construction.</h2>
           <SocialList />
         </div>
       </div>
